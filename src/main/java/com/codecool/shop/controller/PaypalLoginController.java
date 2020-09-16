5a3d9eb09453d0ff8d7f617f065d3dbaef8cc3ef
g-georgia <gg.georgina20@gmail.com>
2020-09-16 12:56:09 +0200

    Remove unnecessary comments

--- src/main/java/com/codecool/shop/controller/PaypalLoginController.java
+++ src/main/java/com/codecool/shop/controller/PaypalLoginController.java
@@ -16,24 +16,9 @@ public class PaypalLoginController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        OrderDao orderDataStore = OrderDaoMem.getInstance();
-//        ProductDao productDataStore = ProductDaoMem.getInstance();
-//        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-//        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-//        List<ProductCategory> allProductCategories = productCategoryDataStore.getAll();
-//
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-//        context.setVariable("category", productCategoryDataStore.getAll());
-//        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-//        context.setVariable("suppliers", supplierDataStore.getAll());
-//        context.setVariable("orderCount", orderDataStore.getAll().size());
 
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
         engine.process("paypal.html", context, resp.getWriter());
 
     }
